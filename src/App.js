09d9230e2c9303ad62bf8b20a3340a6bd991756e
ramirosp <sanchezramiro@hotmail.com>
2021-08-13 09:06:25 -0500

    Proyecto de Ramiro

--- src/App.js
+++ src/App.js
@@ -1,44 +1,76 @@
 //import logo from './logo.svg';
 //import './App.css';
 import {useState} from 'react'
-
-
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+
 
 function App() {
+
+const [showAddTask, setShowAddTask] = useState(false)
 const [tasks, setTasks] = useState([
 {
-  id:1,
-  text:'Cita con el Doctor',
-  day: '25 de Agosto 2021',
-  reminder: true
+id:1,
+text:'Cita con el Doctor',
+day: '25 de Agosto 2021',
+reminder: true
 },
 {
-  id:2,
-  text:'Videoconferencia ITA',
-  day: '3 de Septiembre 2021',
-  reminder: true
+id:2,
+text:'Videoconferencia ITA',
+day: '3 de Septiembre 2021',
+reminder: true
 },
 {
-  id:3,
-  text:'Entrega de Avance de Proyecto',
-  day: '15 de Agosto 2021',
-  reminder: false
-  }
+id:3,
+text:'Entrega de Avance de Proyecto',
+day: '15 de Agosto 2021',
+reminder: false
+}
 ])
 
-const nombre = 'Ricardo'
-const x = true
 
-  return (
-    <div>
-    <Header title='Recordatorios' />
 
-    <Tasks tasks={tasks}/>
 
-    </div>
-  );
+const deleteTask = (id) =>{
+  //console.log('delete', id);
+  setTasks(tasks.filter((task) => task.id !== id))
 }
+
+const toggleReminder = (id =>{
+  //console.log('Reminder', id);
+  setTasks(tasks.map((task) => task.id === id ? {...task,reminder:!task.reminder} : task))
+})
+
+const addTask = (task) => {
+ // console.log(task);
+ const id = Math.floor(Math.random() *10000) + 1
+ const newTask = {id,...task}
+ setTasks([...tasks, newTask])
+
+}
+const nombre = 'Ramiro'
+const x = true
+const y = false
+
+
+
+return (
+<div>
+<Header title='Recordatorios' onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+
+{showAddTask && <AddTask onAdd={addTask} />}
+
+<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+
+
+
+</div>
+);
+}
+
+
 
 export default App;
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,10 @@
 const Task = (props) =>{
-return(
-  <div className='task'>
-    <h3>{props.text}  <button className='delete'>X
+  return(
+    <div className={`task ${props.reminder ? 'reminder' : ''}`} onDoubleClick={() => props.onToggle(props.id)}>
+    <h3>{props.text} <button className='delete' onClick={() => props.onDelete(props.id)}>X
     </button></h3>
     <p>{props.day}</p>
-  </div>
-)
-}
-export default Task
+    </div>
+  )
+  }
+  export default Task
